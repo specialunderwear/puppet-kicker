1db8d06b685a09290e33e8baad5fa40667c92e6c
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-12-03 16:32:00 +0100

    Added function to retrieve only 1 server from the pool, optionally with a fallback.

---
+++ lib/puppet/parser/functions/server_with_role.rb
@@ -0,0 +1,26 @@
+begin
+  require 'puppet/util/log'
+
+  module Puppet::Parser::Functions
+    newfunction(:server_with_role,
+    :type => :rvalue,
+    :doc => "Return one server object with a certain role:
+  
+    server_with_role('frontend', { fallback => 'somevalue' })
+    " ) do |args|
+      Puppet::Parser::Functions.autoloader.loadall
+
+      fallback = nil
+
+      if args.kind_of?(Array) and args.count > 1
+          fallback = args.pop
+      end
+
+      servers = servers_with_role(args)
+      servers.count ? servers : fallback
+    end
+  end
+
+rescue NameError, LoadError
+  # the puppet clients don't have any activerecord. see http://projects.puppetlabs.com/issues/12594
+end
